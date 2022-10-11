b0e96cb8665e58a30d482840ed69088456804f49
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-10-11 20:16:57 +0200

    done

--- sql-practice/phase-1.sql
+++ sql-practice/phase-1.sql
@@ -1,8 +1,41 @@
 --Insert new cat named "Red" born this year
 -- Your code here
+INSERT INTO
+  cats (name, birth_year)
+VALUES
+  ("Red", 2022);
 
 --Assign ownership of new cat to George Beatty using subqueries
 -- Your code here
+INSERT INTO
+  cat_owners (cat_id, owner_id)
+VALUES
+  (
+    (
+      SELECT
+        id
+      from
+        cats
+      WHERE
+        name = 'Red'
+    ),
+    (
+      SELECT
+        id
+      from
+        owners
+      WHERE
+        first_name = "George"
+        AND last_name = "Beatty"
+    )
+  );
 
 --Query to verify INSERTs worked properly
 -- Your code here
+SELECT
+  *
+FROM
+  cat_owners
+WHERE
+  owner_id = 5
+  AND cat_id = 9;
