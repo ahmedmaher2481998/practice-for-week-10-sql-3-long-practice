cdea5fd96cc53a93ea958ae70e47cc24b2ca7782
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-10-11 20:42:40 +0200

    done with phase 3  *-* done

--- sql-practice/phase-3.sql
+++ sql-practice/phase-3.sql
@@ -1,2 +1,47 @@
 -- Find the name of the cats co-owned by both George Beatty and Melynda Abshire
 -- Your code here
+-- select cat id owned by each of them then selecting the union between them 
+SELECT
+  name
+FROM
+  cats
+WHERE
+  id in (
+    SELECT
+      cat_id
+    from
+      cat_owners
+    WHERE
+      cat_id in(
+        SELECT
+          cat_id
+        FROM
+          cat_owners
+        WHERE
+          owner_id = (
+            SELECT
+              id
+            from
+              owners
+            WHERE
+              first_name = "George"
+              AND last_name = "Beatty"
+          )
+      )
+      AND cat_id in (
+        SELECT
+          cat_id
+        FROM
+          cat_owners
+        WHERE
+          owner_id =(
+            SELECT
+              id
+            from
+              owners
+            WHERE
+              first_name = "Melynda"
+              AND last_name = "Abshire"
+          )
+      )
+  )
